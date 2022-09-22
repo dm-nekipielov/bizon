c72d08826db1faccffeaa77e5f7daaf7a83e3b81
dmnekipelov <dm.nekipelov@gmail.com>
2022-09-22 17:12:47 +0300

    Splitted settings added

---
+++ src/config/settings/dev.py
@@ -0,0 +1,9 @@
+from config.settings.base import *  # noqa
+
+CURRENT_ENV = "DEV"
+print(CURRENT_ENV)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
