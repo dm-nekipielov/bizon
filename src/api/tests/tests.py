07cd575b3711a37c990b1186322f8f127743a4eb
dmnekipelov <dm.nekipelov@gmail.com>
2022-10-08 07:41:11 +0300

    Added more api tests

--- src/api/tests/tests.py
+++ src/api/tests/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import (HTTP_200_OK, HTTP_201_CREATED,
+                                   HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED,
+                                   HTTP_404_NOT_FOUND)
 from rest_framework.test import APIClient
 
 from utils import samples
@@ -20,7 +22,7 @@ class TestAPI(TestCase):
         self.user.set_password("qwerty")
         self.user.save()
 
-    def test_product_list(self):
+    def test_product_details(self):
         self.client.force_authenticate(user=self.user)
         response = self.client.get(reverse("api:product_detail", kwargs={"pk": self.product.pk}))
         self.assertEqual(response.status_code, HTTP_200_OK)
@@ -36,3 +38,80 @@ class TestAPI(TestCase):
                 "subcategory": 1,
             },
         )
+
+    def test_product_details_not_authorised(self):
+        response = self.client.get(reverse("api:product_detail", kwargs={"pk": self.product.pk}))
+        self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
+
+    def test_product_not_exist(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(reverse("api:product_detail", kwargs={"pk": 10}))
+        self.assertEqual(response.status_code, HTTP_404_NOT_FOUND)
+
+    def test_product_create(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            reverse("api:product_create"),
+            data={
+                "name": "test_product_2",
+                "price": "1.00",
+                "code": 12345,
+                "description": "Some test description",
+                "stock": 1,
+                "subcategory": 1,
+            },
+        )
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+    def test_product_update_partly(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(
+            reverse("api:product_update", kwargs={"pk": self.product.pk}), data={"name": "new product name"}
+        )
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {
+                "id": 1,
+                "name": "new product name",
+                "price": "999.00",
+                "code": 12345,
+                "description": "Some test description",
+                "stock": 1,
+                "subcategory": 1,
+            },
+        )
+
+    def test_product_update_fully(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(
+            reverse("api:product_update", kwargs={"pk": self.product.pk}),
+            data={
+                "id": 1,
+                "name": "Updated product name",
+                "price": "1.00",
+                "code": 11111,
+                "description": "Updated description",
+                "stock": 1,
+                "subcategory": 1,
+            },
+        )
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {
+                "id": 1,
+                "name": "Updated product name",
+                "price": "1.00",
+                "code": 11111,
+                "description": "Updated description",
+                "stock": 1,
+                "subcategory": 1,
+            },
+        )
+
+    def test_product_delete(self):
+        self.client.force_authenticate(user=self.user)
+        self.product_2 = samples.sample_product(subcategory=self.subcategory, name="test_product_2", price=1)
+        response = self.client.delete(reverse("api:product_delete", kwargs={"pk": self.product_2.pk}))
+        self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
--- src/api/urls.py
+++ src/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import routers, permissions
+from rest_framework import permissions, routers
 
 from api.views import (CategoryCreateView, CategoryDeleteView,
                        CategoryDetailView, CategoryListView,
@@ -16,37 +16,37 @@ routes = routers.DefaultRouter()
 routes.register("customers", UserViewSet)
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Bizon API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Bizon API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 
 urlpatterns = [
     path("", include(routes.urls)),
     path("auth/", include("rest_framework.urls")),
-    path("docs/", schema_view.with_ui('swagger', cache_timeout=0), name='swagger_docs'),
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger_docs"),
     path("auth/", include("djoser.urls.jwt")),
     path("category/", CategoryListView.as_view(), name="category_list"),
     path("category/<int:pk>/", CategoryDetailView.as_view(), name="category_detail"),
-    path("category/create/", CategoryCreateView.as_view(), name="product_create"),
-    path("category/<int:pk>/update/", CategoryUpdateView.as_view(), name="product_create"),
-    path("category/<int:pk>/delete/", CategoryDeleteView.as_view(), name="product_create"),
+    path("category/create/", CategoryCreateView.as_view(), name="category_create"),
+    path("category/<int:pk>/update/", CategoryUpdateView.as_view(), name="category_update"),
+    path("category/<int:pk>/delete/", CategoryDeleteView.as_view(), name="category_delete"),
     path("subcategory/", SubCategoryListView.as_view(), name="subcategory_list"),
     path("subcategory/<int:pk>/", SubCategoryDetailView.as_view(), name="subcategory_detail"),
-    path("subcategory/create/", SubCategoryCreateView.as_view(), name="product_create"),
-    path("subcategory/<int:pk>/update/", SubCategoryUpdateView.as_view(), name="product_create"),
-    path("subcategory/<int:pk>/delete/", SubCategoryDeleteView.as_view(), name="product_create"),
+    path("subcategory/create/", SubCategoryCreateView.as_view(), name="subcategory_create"),
+    path("subcategory/<int:pk>/update/", SubCategoryUpdateView.as_view(), name="subcategory_update"),
+    path("subcategory/<int:pk>/delete/", SubCategoryDeleteView.as_view(), name="subcategory_delete"),
     path("product/", ProductListView.as_view(), name="product_list"),
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("product/create/", ProductCreateView.as_view(), name="product_create"),
-    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_create"),
-    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_create"),
+    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
