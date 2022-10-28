075cc8c1e08be5092a6c5f8ac264f3a2a466f7ad
dmnekipelov <dm.nekipelov@gmail.com>
2022-10-28 15:14:52 +0300

    Nginx added

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -8,6 +8,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATIC_URL = "static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
